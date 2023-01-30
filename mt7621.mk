95567a2d8c7668f3ca57740179f710165866cd74
Joris Lee <lqq@sioiot.com>
2023-01-30 10:51:24 +0800

    Update mt7621.mk

--- mt7621.mk
+++ mt7621.mk
@@ -323,14 +323,14 @@ define Device/d-team_newifi-d2
 	kmod-mt7603 kmod-mt76x2 kmod-usb3 kmod-usb-ledtrig-usbport wpad-basic
 endef
 TARGET_DEVICES += d-team_newifi-d2
-	
-define Device/mediatek-hlk-7621a
-  DTS := HLK-7621A
+
+define Device/mediatek_hlk-mt7621a
+  DTS := HLK-MT7621A
   IMAGE_SIZE := $(ralink_default_fw_size_32M)
-  DEVICE_TITLE := Mediatek HLK-7621A
+  DEVICE_TITLE := Mediatek HLK-MT7621A
   DEVICE_PACKAGES := kmod-mt76x2 kmod-usb3
 endef
-TARGET_DEVICES += mediatek-hlk-7621a
+TARGET_DEVICES += mediatek_hlk-mt7621a
 
 define Device/pbr-m1
   DTS := PBR-M1
